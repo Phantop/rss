2bf1da79d4b6f63aaad16ada64439bf76a8f45d9
Phantop <phantop@tuta.io>
2024-08-22 23:57:44 -0400

    final govtrack bridge

--- bridges/GovTrackBridge.php
+++ bridges/GovTrackBridge.php
@@ -17,6 +17,7 @@ class GovTrackBridge extends BridgeAbstract
                 'Major Legislative Activity' => 'major-bill-activity',
                 'New Bills and Resolutions' => 'introduced-bills',
                 'New Laws' => 'enacted-bills',
+                'Posts from Us' => 'posts'
                 ]
             ],
             'limit' => self::LIMIT
@@ -24,9 +25,46 @@ class GovTrackBridge extends BridgeAbstract
 
     public function collectData()
     {
-        $url = $this->getURI();
-        $html = getSimpleHTMLDOMCached($url);
+        $html = getSimpleHTMLDOMCached($this->getURI());
+        if ($this->getInput('feed') != 'posts') {
+            $this->collectEvent($html);
+            return;
+        }
 
+        $html = defaultLinkTo($html, parent::getURI());
+        $limit = $this->getInput('limit') ?? 10;
+        $count = 0;
+        foreach ($html->find('section') as $element) {
+            $item = [];
+
+            $item['title'] = $element->find('a', 0)->innertext;
+            $item['uri'] = $element->find('a', 0)->href;
+            $item['content'] = $element->find('p', 1);
+
+            $info = explode(' ', $element->find('p', 0)->innertext);
+            $item['timestamp'] = strtotime(implode(' ', array_slice($info, 0, 3)));
+            $item['categories'][] = implode(' ', array_slice($info, 4));
+
+            $html = getSimpleHTMLDOMCached($item['uri']);
+            $html = defaultLinkTo($html, parent::getURI());
+
+            $content = $html->find('#content .col-md', 1);
+            $info = explode(' by ', $content->find('p', 0)->plaintext);
+            $content->removeChild($content->firstChild());
+
+            $item['author'] = implode(' ', array_slice($info, 1));
+            $item['content'] = $content->innertext;
+
+            if ($count++ == $limit) {
+                break;
+            }
+
+            $this->items[] = $item;
+        }
+    }
+
+    private function collectEvent($html)
+    {
         $opt = [];
         preg_match('/"csrfmiddlewaretoken" value="(.*)"/', $html, $opt);
         $header = [
@@ -42,30 +80,27 @@ class GovTrackBridge extends BridgeAbstract
         $opt = [ CURLOPT_POSTFIELDS => $post ];
 
         $html = getContents(parent::getURI() . 'events/_load_events', $header, $opt);
-        $html = defaultLinkTo(str_get_html($html), $url);
+        $html = defaultLinkTo(str_get_html($html), parent::getURI());
 
         foreach ($html->find('.tracked_event') as $event) {
             $item = [];
 
             $bill = $event->find('.event_title a, .event_body a', 0);
-            $item['uri'] = $bill->href;
-            $item['title'] = explode(': ', $bill->innertext)[0];
-            $item['content'] = $event->find('td', 1)->innertext;
-
             preg_match('/Sponsor:(.*)\n/', $event->plaintext, $opt);
-            $item['author'] = $opt[1] ?? '';
-
             $date = explode(' ', $event->find('.event_date', 0)->plaintext);
+
+            $item['author'] = $opt[1] ?? '';
+            $item['content'] = $event->find('td', 1)->innertext;
+            $item['enclosures'][] = $event->find('img', 0)->src;
             $item['timestamp'] = strtotime(implode(' ', array_slice($date, 2)));
+            $item['title'] = explode(': ', $bill->innertext)[0];
+            $item['uri'] = $bill->href;
 
             foreach ($event->find('.event_title, .event_type span') as $tag) {
                 if (!$tag->find('a', 0)) {
                     $item['categories'][] = $tag->plaintext;
                 }
             }
-
-            $item['enclosures'][] = $event->find('img', 0)->src;
-
             $this->items[] = $item;
         }
     }
@@ -81,7 +116,11 @@ class GovTrackBridge extends BridgeAbstract
 
     public function getURI()
     {
-        $url = parent::getURI() . 'events/' . $this->getInput('feed');
+        if ($this->getInput('feed') != 'posts') {
+            $url = parent::getURI() . 'events/' . $this->getInput('feed');
+        } else {
+            $url = parent::getURI() . $this->getInput('feed');
+        }
         return $url;
     }
 }
