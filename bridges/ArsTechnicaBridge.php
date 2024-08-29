130af5fac8abcf29deb905a837dc260f0d9c7e81
Phantop <phantop@tuta.io>
2024-08-28 22:34:32 -0400

    clean ars bridge more

--- bridges/ArsTechnicaBridge.php
+++ bridges/ArsTechnicaBridge.php
@@ -35,35 +35,27 @@ class ArsTechnicaBridge extends FeedExpander
 
     protected function parseItem(array $item)
     {
+        $item_html = getSimpleHTMLDOMCached($item['uri'] . '&comments=1');
+        $parsely = $item_html->find('[name="parsely-page"]', 0)->content;
+        $parsely_json = Json::decode(html_entity_decode($parsely));
+
+        $item['categories'] = $parsely_json['tags'];
+        $item['comments'] = $item_html->find('#view_comments', 0)->href;
+        $item['content'] = $item_html->find('.article-content', 0);
         $item['uid'] = explode('=', $item['uri'])[1];
 
-        $item_html = getSimpleHTMLDOMCached($item['uri'] . '&comments=1');
-        $item_html = defaultLinkTo($item_html, self::URI);
-        $item['content'] = $item_html->find('.article-content', 0);
-        $item['comments'] = $item_html->find('#view_comments', 0)->href;
-
-        $pages = $item_html->find('nav.page-numbers > .numbers > a', -2);
-        if ($pages) {
-            for ($i = 2; $i <= $pages->innertext; $i++) {
-                $page_url = $item['uri'] . '&page=' . $i;
-                $page_html = getSimpleHTMLDOMCached($page_url);
-                $page_html = defaultLinkTo($page_html, self::URI);
-                $item['content'] .= $page_html->find('.article-content', 0);
-            }
-            $item['content'] = str_get_html($item['content']);
+        $pages = $item_html->find('.page-numbers a');
+        foreach (array_splice($pages, -1) as $page) {
+            $page_html = getSimpleHTMLDOMCached($page->href);
+            $item['content'] .= $page_html->find('.article-content', 0);
         }
+        $item['content'] = backgroundToImg($item['content']);
 
         // remove various ars advertising
         $sel = '#social-left, .ars-component-buy-box, .ad_wrapper, .sidebar';
         foreach ($item['content']->find($sel) as $ad) {
             $ad->remove();
         }
-
-        $item['content'] = backgroundToImg($item['content']);
-
-        $parsely = $item_html->find('[name="parsely-page"]', 0)->content;
-        $parsely_json = json_decode(html_entity_decode($parsely), true);
-        $item['categories'] = $parsely_json['tags'];
 
         return $item;
     }
